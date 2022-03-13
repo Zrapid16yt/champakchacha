159c3a115d4689edd692a6d23ac87f7a9e32f1c7
Zrapid16yt <98387004+Zrapid16yt@users.noreply.github.com>
2022-03-13 21:01:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function task(i) {
 	
   setTimeout(function() { 
     if(first == true){
-	bot.chat("Subscribe To Easy Gamer : https://www.youtube.com/channel/UCk575VDlrCsv1Gy2XZQ1bSA")
+	bot.chat("Subscribe To Zrapid16 : https://www.youtube.com/channel/UCEEZI9peDuALLKUvk3GQ7GA")
 	first = false;
 	}
 	else{
-		bot.chat("Subscribe To MannuG : https://www.youtube.com/channel/UCSZHRnBue50ImBKeR6k6eDg")
+		bot.chat("Subscribe To Zrapid16 : https://www.youtube.com/channel/UCEEZI9peDuALLKUvk3GQ7GA")
 		first = true;
 	}
   }, 3600000 * i); 
